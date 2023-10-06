75da32da22a229f9fbc28c79bb2b8c2884e50d7c
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-10-06 13:08:02 +0530

    Day02

---
+++ Typescript/Day02/demo1.ts
@@ -0,0 +1,24 @@
+console.log("Hello World...");
+console.log('Welcome to Angualr 12 Module')
+console.log(`
+        Hopes So U r Enjoying This Module....
+`);
+
+var a1,a2;
+var a_1,a$1;
+var s;
+// data type
+// number(int,long,double,float,short),string(''/""),boolean(true/false)
+// any (number,string,boolean)
+// void (it does not point anything => neutral data type )
+// misc null & undefined => it used value as well as data type 
+
+var b:number=10;   // Forward declration & defination 
+console.log("value of b is "+b);
+console.log('value of b is '+b);
+console.log(`
+        Value of B is ${b}
+`);
+
+
+
