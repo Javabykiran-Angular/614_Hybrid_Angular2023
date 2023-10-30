997fa4c3f023e04ed5803c8da8a2a0436cc41ba0
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-10-30 12:50:18 +0530

    Day04

---
+++ Angular/Day04/propertybinding/propertybinding.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-propertybinding',
+  templateUrl: './propertybinding.component.html',
+  styleUrls: ['./propertybinding.component.css']
+})
+export class PropertybindingComponent implements OnInit {
+
+
+    name:string='Sumit Raokhande';
+
+    imgUrl='../../assets/bg4.jpg';
+    isshow:boolean=false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular/Day04/stringinterpolation/stringinterpolation.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stringinterpolation',
+  templateUrl: './stringinterpolation.component.html',
+  styleUrls: ['./stringinterpolation.component.css']
+})
+export class StringinterpolationComponent implements OnInit {
+
+  name:string='Sumit Raokhande';
+  imgUrl:string='../../assets/bg4.jpg';
+  num:number=4;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
