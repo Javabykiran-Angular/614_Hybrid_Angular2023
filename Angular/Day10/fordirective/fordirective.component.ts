b466e60cbfd8faadbf1ba77a4b17d5ec6d3eeaea
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-11-07 12:53:14 +0530

    Day10

---
+++ Angular/Day10/fordirective/fordirective.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fordirective',
+  templateUrl: './fordirective.component.html',
+  styleUrls: ['./fordirective.component.css']
+})
+export class FordirectiveComponent implements OnInit {
+
+  course:string[]=['Core Java','Advanced java','HTML,CSS','Spring Boot','Hibernate','Angular 12','AWS','Docker'];
+
+  arrObj=[
+    {
+      name:'Samsung',
+      price:25000,
+      qty:1
+    },
+    {
+      name:'Apple',
+      price:65000,
+      qty:1
+    },
+    {
+      name:'Realme',
+      price:18000,
+      qty:1
+    },
+    {
+      name:'Motorolla',
+      price:30000,
+      qty:1
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
